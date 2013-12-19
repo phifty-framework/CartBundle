c7543647c4cc8a0bb572aed0d3b14ed42b14d8c6
c9s <cornelius.howl@gmail.com>
2013-12-20 00:54:41 +0800

    Add RemoveCartItem action

---
+++ Action/RemoveCartItem.php
@@ -0,0 +1,19 @@
+<?php
+namespace CartBundle\Action;
+use ActionKit\Action;
+use CartBundle\Cart;
+
+class RemoveCartItem extends Action
+{
+    public function schema() {
+        $this->param('id')
+            ->label( _('項目 ID') )
+            ->required();
+    }
+
+    public function run() {
+        $cart = Cart::getInstance();
+        $cart->removeItem( intval($this->arg('id')) );
+        return $this->success( _('已從購物車移除') );
+    }
+}
