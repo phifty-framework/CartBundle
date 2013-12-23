b5d91c53c91d72cd2b89ce0ba28e9ae6accfd988
c9s <cornelius.howl@gmail.com>
2013-12-23 10:36:24 +0800

    check error

--- Action/Checkout.php
+++ Action/Checkout.php
@@ -43,6 +43,7 @@ class Checkout extends CreateRecordAction
                 }
                 if ( strlen(trim($this->arg('utc'))) != 8 ) {
                     $this->invalidField('utc', _('統一編號必須是八碼，再麻煩您檢查一下') );
+                    return $this->error(_('發票欄位填寫錯誤'));
                 }
             }
         }
@@ -67,6 +68,9 @@ class Checkout extends CreateRecordAction
             if ( ! parent::run() ) {
                 throw new Exception( _('無法建立訂單') );
             }
+            if ( ! $this->record->id ) {
+                throw new Exception( _('無法建立訂單項目') );
+            }
             foreach( $orderItems as $orderItem ) {
                 $ret = $orderItem->update([
                     'order_id' => $this->record->id,
@@ -85,7 +89,7 @@ class Checkout extends CreateRecordAction
             return $this->redirectLater('/order/view?' . http_build_query([
                 'o' => $this->record->id,
                 't' => $this->record->token,
-            ]), 3);
+            ]), 2);
         } catch ( Exception $e ) {
             kernel()->db->rollback();
             return $this->error( $e->getMessage() );
