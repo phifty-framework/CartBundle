845319337cbcef989819d55890bb3e5238cda58a
c9s <cornelius.howl@gmail.com>
2013-12-23 10:11:06 +0800

    Add ChoosePaymentType action

---
+++ Action/ChoosePaymentType.php
@@ -0,0 +1,26 @@
+<?php
+namespace CartBundle\Action;
+use ActionKit\Action;
+use CartBundle\Model\Order;
+
+class ChoosePaymentType extends Action
+{
+    public function schema() {
+        $this->param('o')->required()->label( '訂單編號' );
+        $this->param('t')->required()->label( 'Security Token' );
+        $this->param('payment_type')->required();
+    }
+
+    public function run() {
+        $order = new Order;
+        $order->load([ 'id' => $this->arg('o') , 'token' => $this->arg('t') ]);
+        if ( ! $order->id ) {
+            return $this->error( _('錯誤') );
+        }
+        $ret = $order->update([ 'payment_type' => $this->arg('payment_type') ]);
+        if ( ! $ret->success ) {
+            return $this->error( $ret->message );
+        }
+        return $this->success('設定成功');
+    }
+}
