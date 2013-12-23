98b9bebda0121b0e074ff1abeb7c2f005a27445c
c9s <cornelius.howl@gmail.com>
2013-12-24 00:19:54 +0800

    sort listColumns

--- OrderCRUDHandler.php
+++ OrderCRUDHandler.php
@@ -9,7 +9,16 @@ class OrderCRUDHandler extends CRUDHandler
     public $modelClass = 'CartBundle\Model\Order';
     public $crudId     = 'order';
 
-    public $listColumns = array( 'id', 'sn', 'buyer_name', 'buyer_cellphone' , 'payment_type' , 'payment_status' , 'total_amount', 'paid_amount' , 'created_on' );
+    public $listColumns = array(
+        'id', 'sn', 'buyer_name', 'buyer_cellphone' , 
+        'payment_type', 
+        'payment_status',
+        'coupon_code' ,
+        'total_amount', 
+        'discount_amount', 
+        'paid_amount', 
+        'created_on',
+    );
 
     public $quicksearchFields = array('buyer_name', 'buyer_phone', 'buyer_email');
 
