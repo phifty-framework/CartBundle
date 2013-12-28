5390e6242d97cd3d7151e1750614c3e2e40fc4df
c9s <cornelius.howl@gmail.com>
2013-12-29 01:22:56 +0800

    send customer question email

--- Action/UpdateCustomerQuestion.php
+++ Action/UpdateCustomerQuestion.php
@@ -3,6 +3,7 @@ namespace CartBundle\Action;
 use ActionKit\Action;
 use ActionKit\RecordAction\UpdateRecordAction;
 use CartBundle\Model\CustomerQuestion;
+use CartBundle\Email\CustomerQuestionReplyEmail;
 
 class UpdateCustomerQuestion extends UpdateRecordAction
 {
@@ -13,7 +14,13 @@ class UpdateCustomerQuestion extends UpdateRecordAction
         if ( ! $cUser->isLogged() || ! $cUser->hasRole('admin') ) {
             return $this->error('無此權限');
         }
-        return parent::run();
+        $ret = parent::run();
+        if ( $ret ) {
+            $question = $this->getRecord();
+            $email = new CustomerQuestionReplyEmail($question->member, $question);
+            $email->send();
+        }
+        return $ret;
     }
 
     public function successMessage($ret) {
