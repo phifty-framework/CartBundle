d0d9776bc4e89328f23968028d0b3fe1478a648f
c9s <cornelius.howl@gmail.com>
2014-03-02 15:37:14 +0800

    fix

--- Model/CustomerQuestion.php
+++ Model/CustomerQuestion.php
@@ -3,6 +3,9 @@ namespace CartBundle\Model;
 
 class CustomerQuestion  extends \CartBundle\Model\CustomerQuestionBase {
 
+    public function getLabel() {
+        return _('客服問答');
+    }
     public function beforeUpdate($args) {
         if ( isset($args['question']) && $args['question'] ) {
             $args['question_time'] = date('c');
