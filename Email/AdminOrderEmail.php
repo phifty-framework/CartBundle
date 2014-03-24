3ef30aaec0902311450ace14602ccefcd4ca232a
c9s <yoanlin93@gmail.com>
2014-03-24 19:25:59 +0800

    PaymentATMAdminEmail

---
+++ Email/AdminOrderEmail.php
@@ -0,0 +1,32 @@
+<?php
+namespace CartBundle\Email;
+use Phifty\Message\Email;
+use UserBundle\Email\AdminEmail;
+
+/**
+ * Order Shipping Notification Email
+ *
+ *     $email = new MemberOrderEmail($member, $order)
+ *     $email->send();
+ */
+class AdminOrderEmail extends AdminEmail
+{
+    public $order;
+    public $member;
+
+    public function __construct($member, $order)
+    {
+        $this->member = $this['member'] = $member;
+        $this->order = $this['order'] = $order;
+        parent::__construct();
+    }
+
+    public function from() {
+         return kernel()->getSystemMail();
+    }
+}
+
+
+
+
+
