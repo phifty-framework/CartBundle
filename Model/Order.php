de3a15098dd764848598efa23609656f4b675dff
azole <azolelai@gmail.com>
2015-07-03 15:37:10 +0800

    update for ActionKit: add CSRFToken, refactor addRecordAction and rename action to __action

--- Model/Order.php
+++ Model/Order.php
@@ -117,11 +117,4 @@ class Order  extends \CartBundle\Model\OrderBase {
     public function delete() {
         return $this->update(array('is_deleted' => true));
     }
-
-    public function rowClass() {
-        if ($this->is_deleted) {
-            return 'deleted';
-        }
-        return '';
-    }
 }
